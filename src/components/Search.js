c1f398dbe047ca662cfa7ede886d2b1c79c1528c
Lars Wallenborn <lars@wallenborn.net>
2017-12-26 19:22:38 +0100

    show placeholder in search box

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,11 @@ import Input from "./ui/Input";
 export default class Search extends React.Component {
   render() {
     return (
-      <Input return={this.props.return} type="text" placeholder="sha256, md5 or sha1"/>
+      <Input onKeyUp={(event) => {
+        if (event.keyCode === 13) {
+          if (this.props.return) this.props.return(event.target.value);
+        }
+      }} type="text" placeholder="sha256, md5 or sha1"/>
     );
   }
 }
--- src/components/ui/Input.js
+++ src/components/ui/Input.js
@@ -4,11 +4,7 @@ import {css} from "glamor";
 export default class Input extends React.Component {
   render() {
     return (
-      <input onKeyUp={(event) => {
-        if (event.keyCode === 13) {
-          if (this.props.return) this.props.return(event.target.value);
-        }
-      }} {...style}/>
+      <input {...style} {...this.props}/>
     );
   }
 }
