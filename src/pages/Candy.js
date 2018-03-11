cded87897d9069571c8664b114a80bb9ba38ad86
Lars Wallenborn <lars@wallenborn.net>
2018-03-11 17:56:35 +0100

    includes word clouds

--- src/pages/Candy.js
+++ src/pages/Candy.js
@@ -2,13 +2,37 @@ import React from "react";
 import {withRouter} from 'react-router';
 import BuildTimeStampChart from "../components/BuildTimeStampChart";
 import ProcessingByMonthChart from "../components/ProcessingByMonthChart";
+import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
 
 class Candy extends React.Component {
   render() {
-    return <div>
-      <BuildTimeStampChart/>
-      <ProcessingByMonthChart/>
-    </div>;
+    return <Tabs>
+      <TabList>
+        <Tab>Build Timestamps</Tab>
+        <Tab>Processing</Tab>
+        <Tab>Wordclouds</Tab>
+      </TabList>
+
+      <TabPanel>
+        <BuildTimeStampChart/>
+      </TabPanel>
+      <TabPanel>
+        <ProcessingByMonthChart/>
+      </TabPanel>
+      <TabPanel>
+        <p>Beautiful and mildly informative.</p>
+        <Tabs>
+          <TabList>
+            <Tab>PDB paths</Tab>
+            <Tab>Sections</Tab>
+            <Tab>Resources</Tab>
+          </TabList>
+          <TabPanel><img src="/image/pdb-paths.png" width="100%"/></TabPanel>
+          <TabPanel><img src="/image/section-names.png" width="100%"/></TabPanel>
+          <TabPanel><img src="/image/resource-names.png" width="100%"/></TabPanel>
+        </Tabs>
+      </TabPanel>
+    </Tabs>;
   }
 }
 
